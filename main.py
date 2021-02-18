98968e36a7404dfae6feb7639ba941be2e81a7b9
Yitang Chen <yc4120@ic.ac.uk>
2021-02-18 15:20:08 +0800

    Add interaction

--- main.py
+++ main.py
@@ -15,11 +15,32 @@ def narancia_calculator(n1: int, n2: int) -> int:
     part2 = functools.reduce(operator.mul, map(lambda x: int(x), str(n2)))
     # 错位相加
     res = part1 + 10 * part2
-    # ；如果末尾有零，福葛好像教过零等于没有，那就划掉吧～
+    # 如果末尾有零，福葛好像教过零等于没有，那就划掉吧～
     while res % 10 == 0:
         res /= 10
     return int(res)
 
 
+def int_in(error_msg: str) -> int:
+    try:
+        return int(input())
+    except:
+        print(error_msg)
+        int_in(error_msg)
+
+
+def interact():
+    while True:
+        print("请输入被乘数/Please enter the multiplicand/被乗数を入力してください：")
+        n1 = int_in("请输入有效的整数，否则天才儿童无法理解/Please enter a valid integer, otherwise, Narancia cannot "
+                    "understand it/有効な整数を入力してください、またはナランチャは理解できません：")
+        print("请输入乘数/Please enter the multiplier/乗数を入力してください：")
+        n2 = int_in("请输入有效的整数，否则天才儿童无法理解/Please enter a valid integer, otherwise, Narancia cannot "
+                    "understand it/有効な整数を入力してください、またはナランチャは理解できません：")
+        print(f"{n1} * {n2} = {narancia_calculator(n1, n2)}.")
+        print("当たってる？")
+
+
 if __name__ == '__main__':
-    print(narancia_calculator(16, 55))
+    # print(narancia_calculator(16, 55))  # 28
+    interact()
